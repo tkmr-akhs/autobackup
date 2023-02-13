8be835da37e832a9a6c370c791499cc041cd6180
TAKAMURA Akihisa <tkmr.akhs@gmail.com>
2023-02-13 17:59:18 +0900

    Refactoring
    Minor update in build task.

--- build_env/build.bat
+++ build_env/build.bat
@@ -1,10 +1,11 @@
 deactivate
+RMDIR /s /q build_env\.venv
+
+COPY /y pyproject.toml build_env\
+COPY /y poetry.lock build_env\
+
 CD build_env
 
-COPY /y ..\pyproject.toml .\
-COPY /y ..\poetry.lock .\
-
-RMDIR /s /q .venv
 poetry install --only main
 
 CD ..
@@ -14,8 +15,4 @@ MKDIR dist\cnf
 COPY /y src\cnf\defaults.toml dist\cnf\
 COPY /y src\cnf\logging.toml dist\cnf\
 
-poetry build
-
-for /f "usebackq delims=" %%A in (`dir /b dist\autobackup-*.whl`) do set WHL_FILE=%%A
-
-CALL build_env\.venv\Scripts\activate.bat & pip install dist\%WHL_FILE% & pyinstaller src\main.py --name autobackup --onefile --clean & deactivate
+CALL build_env\.venv\Scripts\activate.bat & pyinstaller src\main.py --name autobackup --onefile --clean & deactivate
